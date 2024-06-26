5e72984d2d22896f0cc75053333997937310c168
Brian Inca <brianincah10@gmail.com>
2024-06-26 12:27:55 -0500

    Proyecto OK

---
+++ app/src/main/java/com/example/semana11_dam/MyNotaRecyclerViewAdapter.java
@@ -0,0 +1,72 @@
+package com.example.semana11_dam;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyNotaRecyclerViewAdapter extends RecyclerView.Adapter<MyNotaRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Nota> mValues;
+    private final NotasInteractionListener mListener;
+    public MyNotaRecyclerViewAdapter(List<Nota> notaList, NotasInteractionListener mListener) {
+        this.mValues = notaList;
+        this.mListener = mListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+
+                .inflate(R.layout.fragment_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.tvTitulo.setText(holder.mItem.getTitulo());
+        holder.tvContenido.setText(holder.mItem.getContenido());
+        if(holder.mItem.isFavorita()) {
+            holder.ivFavorita.setImageResource(R.drawable.baseline_star_border_24);
+        }
+        holder.ivFavorita.setOnClickListener(v->{
+            if(null != mListener){
+                mListener.favoritaNotaClick(holder.mItem);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public View mView;
+
+        public TextView tvTitulo = null;
+        public TextView tvContenido = null;
+        public ImageView ivFavorita;
+        public Nota mItem;
+
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            mView = view;
+            this.tvTitulo = view.findViewById(R.id.textViewTitulo);
+            this.tvContenido = view.findViewById(R.id.textViewContenido);
+            this.ivFavorita = view.findViewById(R.id.imageViewFavorita);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + tvTitulo.getText() + "'";
+        }
+    }
+}
---
+++ app/src/main/java/com/example/semana11_dam/NotasInteractionListener.java
@@ -0,0 +1,8 @@
+package com.example.semana11_dam;
+
+public interface NotasInteractionListener {
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+}
+
